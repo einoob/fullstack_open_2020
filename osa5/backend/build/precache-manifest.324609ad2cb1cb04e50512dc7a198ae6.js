d47c199b208d8206408ff4c12d4cc780c92974ae
Eino Lindberg <eino.lindberg@gmail.com>
2020-12-15 16:38:04 +0200

    15.12.2020

---
+++ osa5/backend/build/precache-manifest.324609ad2cb1cb04e50512dc7a198ae6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a607672dbc1814337f75edecc684b4e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2af5899915d12146f050",
+    "url": "/static/js/2.29fe1056.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.29fe1056.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdc69e37764d3e4ae408",
+    "url": "/static/js/main.4776e483.chunk.js"
+  },
+  {
+    "revision": "7eb8d6388ecf90523c80",
+    "url": "/static/js/runtime-main.7dc31f0f.js"
+  }
+]);
--- osa5/frontend/src/App.js
+++ osa5/frontend/src/App.js
@@ -1,18 +1,100 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
+	const [blogs, setBlogs] = useState([])
+	const [newBlog, setNewBlog] = useState('')
+	const [errorMessage, setErrorMessage] = useState(null)
+	const [username, setUsername] = useState('')
+	const [password, setPassword] = useState('')
+	const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
     )  
-  }, [])
+	}, [])
+
+	useEffect(() => {
+		const loggedUserJSON = window.localStorage.getItem('loggedBlogger')
+		if (loggedUserJSON) {
+			const user = JSON.parse(loggedUserJSON)
+			setUser(user)
+			blogService.setToken(user.token)
+		}
+	}, [])
+
+	const loginForm = () => {
+		return (
+		<form onSubmit={handleLogin}>
+			<div>
+				username
+				<input
+				type="text"
+				value={username}
+				name="Username"
+				onChange={({ target}) => setUsername(target.value)}
+				/>
+			</div>
+			<div>
+				password
+				<input
+				type="password"
+				value={password}
+				name="Password"
+				onChange={({ target }) => setPassword(target.value)}
+				/>
+			</div>
+			<button type="submit">login</button>
+		</form>
+		)
+	}
+
+	const blogForm = () => {
+		return (
+		<div>
+			placeboholder
+		</div>
+		)
+		/*<form onSubmit={addBlog}>
+				<input
+				value={newBlog}
+				onChange={handleBlogChange}
+				/>
+				<button type="submit">save</button>
+			</form>*/
+		}
+		
+	
+	const handleLogin = async (event) => {
+		event.preventDefault()
+		console.log('logging with', username, password)
+	try {
+		const user = await loginService.login({
+			username, password
+		})
+		window.localStorage.setItem(
+			'loggedBlogger', JSON.stringify(user)
+		)
+		blogService.setToken(user.token)
+		setUser(user)
+		setUsername('')
+		setPassword('')
+	} catch (exception) {
+		setErrorMessage('wrong credentials')
+		setTimeout(() => {
+			setErrorMessage(null)
+		}, 5000)
+	}
+}
 
   return (
     <div>
+			<h1>Keijon pislablogit</h1>
+			{user === null && loginForm(username, setUsername, password, setPassword, handleLogin)}
+			{user !== null && blogForm()}
       <h2>blogs</h2>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
